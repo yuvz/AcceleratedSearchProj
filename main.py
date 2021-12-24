215fe58d0e3fec47dc32cb1e210c28dbd05a426c
yuvz <yuvhad1@hotmail.com>
2021-12-24 11:13:09 +0200

    Create initial infrastructure for database building

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def generate_example(warehouse, algorithm_name, is_show_animation=True, is_show_
         show_plan_as_image(warehouse, plan, running_time, algorithm_name, title, is_export_visualization)
 
 
-def generate_routes(warehouse, algorithm_name, source_id=0, destination_id=0, generate_for_all_combinations=False):
+def generate_routes(warehouse, algorithm_name="MPR_WS", source_id=0, destination_id=0, generate_for_all_combinations=False):
     source, destination = warehouse.sources[source_id], warehouse.destinations[destination_id]
     routes = create_routes_from_source_to_destination_by_MPR_WS(warehouse, source, destination)
     # TODO: export_routes_to_csv(routes, algorithm_name) - @NimrodMarom
