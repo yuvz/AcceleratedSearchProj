ac504dfcd436b2e87ca86d8154a5c2de91eead0a
Nimrod <nimrod.marom@gmail.com>
2021-12-25 22:58:14 +0200

    DataBaseBuilding.py edit

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from DatabaseBuilding import create_routes_from_source_to_destination_by_MPR_WS, export_routes_to_csv
+from DatabaseBuilding import create_routes_from_source_to_destination_by_MPR_WS, export_routes_to_csv, export_warehouse_to_csv, import_csv_to_routes
 from ExampleGeneration import generate_rand_example
 from EnvironmentUtils import generate_warehouse, generate_rand_routing_requests
 from Visualization import show_plan_as_animation, show_plan_as_image
-
+    
 RANDOM_SCHEDULING_ENABLED = False
 
 
 def generate_example(warehouse, algorithm_name, is_show_animation=True, is_show_image=False,
-                     is_export_visualization=False):
+                     is_export_visualization=False): 
     plan, running_time = generate_rand_example(warehouse, algorithm_name)
     title = "Visualization title"
 
@@ -31,12 +31,13 @@ def main():
     Supported values for algorithm_name: [BFS, RND, LNS_RND, ROR, k-ROR, IPWS, k-IPWS, MPR, k-MPR, MPR_WS, k-MPR_WS,
     CBS] - check generate_example() in ExampleGeneration.py to see which algorithm is referred to by each abbr.
     """
-
     warehouse_types = {"first paper": 1, "toy": 2, "small structured": 3, "small empty single origin": 4}
     warehouse = generate_warehouse(warehouse_types["small structured"])
+    
+    export_warehouse_to_csv(warehouse)
 
     generate_example(warehouse, "MPR_WS")
-    # generate_routes(warehouse, "MPR_WS", 2, 1)
+    generate_routes(warehouse, "MPR_WS", 2, 1)
 
     # plan, running_time = generate_rand_example(warehouse, algorithm_name)
 
